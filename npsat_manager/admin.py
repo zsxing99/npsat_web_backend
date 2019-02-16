a456aa5d81e21dec3103827062116d79a208f899
Nick <ultraayla@gmail.com>
2019-02-15 16:06:14 -0800

    Made counties get enabled by default if we have code for them, added models to admin

--- npsat_manager/admin.py
+++ npsat_manager/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import County, Crop, CropGroup
+
+admin.site.register(County)
+admin.site.register(Crop)
+admin.site.register(CropGroup)
--- npsat_manager/load_data.py
+++ npsat_manager/load_data.py
@@ -34,3 +34,16 @@ def load_counties():
 		for county in counties:
 			models.County(name=county['name'], ab_code=county['abcode'], ansi_code=county['ansi']).save()
 
+	enable_default_counties()
+
+
+def enable_default_counties(enable_counties=("Tulare",)):
+	"""
+		By default, we consider counties inactive so they don't show in the list if we can't use them.
+	:return:
+	"""
+
+	for county in enable_counties:
+		update_county = models.County.objects.get(name="county")
+		update_county.active_in_npsat = True
+		update_county.save()
